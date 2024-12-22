988c33e02276e9ec217cb9260aaee1a931e19d0e
withsme <tmdal9953@gmail.com>
2024-12-22 12:31:29 +0900

    feat : Texture 비교

--- src/index.js
+++ src/index.js
@@ -23,51 +23,54 @@ if (WEBGL.isWebGLAvailable()) {
   pointLight.position.set(0, 2, 12)
   scene.add(pointLight)
 
+  // 텍스처
+  const textureLoader = new THREE.TextureLoader()
+  const textureBaseMap = textureLoader.load('../static/img/stone_basecolor.png');
+  const textureNormalMap = textureLoader.load('../static/img/stone_normal.png');
+  const textureHeightMap = textureLoader.load('../static/img/stone_height.png');
+  const textureRoughnessMap = textureLoader.load('../static/img/stone_roughness.png');
+
   // 도형
-  const geometry01 = new THREE.TorusGeometry(0.3, 0.15, 16, 40)
-  const material01 = new THREE.MeshBasicMaterial({ // MeshBasicMaterial은 빛 영향을 안받음
-    color: 0xFF7F00
+  const geometry01 = new THREE.SphereGeometry(0.3, 32, 16)
+  const material01 = new THREE.MeshStandardMaterial({ 
+    map: textureBaseMap
   })
   const obj01 = new THREE.Mesh(geometry01, material01)
-  obj01.position.x = -2
+  obj01.position.x = -1.5
   scene.add(obj01)
   
-  const geometry02 = new THREE.TorusGeometry(0.3, 0.15, 16, 40)
+  const geometry02 = new THREE.SphereGeometry(0.3, 32, 16)
   const material02 = new THREE.MeshStandardMaterial({
-    color: 0xFF7F00,
-    metalness: 0.6,
-    roughness: 0.4,
+    map: textureBaseMap,
+    normalMap: textureNormalMap
   })
   const obj02 = new THREE.Mesh(geometry02, material02)
-  obj02.position.x = -1
+  obj02.position.x = -0.5
   scene.add(obj02)
 
-  const geometry03 = new THREE.TorusGeometry(0.3, 0.15, 16, 40)
-  const material03 = new THREE.MeshPhysicalMaterial({
-    color: 0xFF7F00,
-    clearcoat: 1,
-    clearcoatRoughness: 0.1
+  const geometry03 = new THREE.SphereGeometry(0.3, 32, 16)
+  const material03 = new THREE.MeshStandardMaterial({
+    map: textureBaseMap,
+    normalMap: textureNormalMap,
+    displacementMap: textureHeightMap,
+    displacementScale: 0.05
   })
   const obj03 = new THREE.Mesh(geometry03, material03)
+  obj03.position.x = 0.5
   scene.add(obj03)
 
-  const geometry04 = new THREE.TorusGeometry(0.3, 0.15, 16, 40)
-  const material04 = new THREE.MeshLambertMaterial({
-    color: 0xFF7F00
+  const geometry04 = new THREE.SphereGeometry(0.3, 32, 16)
+  const material04 = new THREE.MeshStandardMaterial({
+    map: textureBaseMap,
+    normalMap: textureNormalMap,
+    displacementMap: textureHeightMap,
+    displacementScale: 0.05,
+    roughnessMap: textureRoughnessMap,
+    roughness: 0.5
   })
   const obj04 = new THREE.Mesh(geometry04, material04)
-  obj04.position.x = 1
+  obj04.position.x = 1.5
   scene.add(obj04)
-
-  const geometry05 = new THREE.TorusGeometry(0.3, 0.15, 16, 40)
-  const material05 = new THREE.MeshPhongMaterial({
-    color: 0xFF7F00,
-    shininess: 60,
-    specular: 0x004fff
-  })
-  const obj05 = new THREE.Mesh(geometry05, material05)
-  obj05.position.x = 2
-  scene.add(obj05)
 
   function render(time) {
     time *= 0.001
@@ -76,7 +79,6 @@ if (WEBGL.isWebGLAvailable()) {
     obj02.rotation.y = time
     obj03.rotation.y = time
     obj04.rotation.y = time
-    obj05.rotation.y = time
 
     renderer.render(scene, camera)
     requestAnimationFrame(render)
